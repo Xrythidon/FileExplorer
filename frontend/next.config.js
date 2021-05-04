a95ae29067128fc79d81f7e828ea033e18d73758
Alex Danisz <alexdanisz@live.com>
2021-05-04 13:25:44 -0700

    Added production env var for vercel

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -10,7 +10,7 @@ module.exports = {
   env: {
     DEV_API_HOST: "http://localhost:5000",
     PROD_API_HOST: "https://breadcrumbfilexplorer.herokuapp.com",
-    NEXT_ENV: "development",
+    NEXT_ENV: "production",
   },
   async rewrites() {
     return [
